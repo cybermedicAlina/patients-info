461f9ebc1d91c015258ed52dd8ce9c8dce3acce8
Evgenia98 <77078700+Evgenia98@users.noreply.github.com>
2021-01-20 22:26:56 +0300

    Add files via upload

---
+++ nevrology1.sql
@@ -0,0 +1,66 @@
+create table patients
+(
+    id                        INTEGER
+        primary key autoincrement,
+    name                      TEXT,
+    gender                    TEXT,
+    diagnosis                 TEXT,
+    type_of_epileptic_seizure TEXT,
+    age_of_onset              TEXT,
+    address                   TEXT,
+    date_of_examination       NUMERIC,
+    age                       INTEGER,
+    date_of_birth             NUMERIC,
+    pathogenic_mutation       TEXT,
+    complaints                TEXT,
+    test_results              TEXT,
+    case_history              TEXT,
+    perinatal_history         TEXT,
+    childbirth                TEXT,
+    birth_weight              TEXT,
+    APGAR                     TEXT,
+    development_formula       TEXT,
+    speech                    TEXT,
+    heredity                  TEXT,
+    in_neurological_status    TEXT,
+    body_mass                 TEXT,
+    doctors_conclusion        TEXT,
+    recommendations           TEXT
+);
+
+
+create table antiepileptic_drug
+(
+    id        INTEGER
+        primary key,
+    drug_name TEXT
+);
+
+create table antiepileptic_drug_patients
+(
+    id_patients      INTEGER
+        references patients,
+    id_drug          INTEGER
+        references antiepileptic_drug,
+    dose             TEXT,
+    side_effect      TEXT,
+    treatment_effect TEXT
+);
+
+create table EEG
+(
+    date        NUMERIC,
+    result      TEXT,
+    id_patients INTEGER
+        references patients
+);
+
+create table results_of_instrumental_research
+(
+    id_patients      INTEGER
+        references patients,
+    date             NUMERIC,
+    type_of_research TEXT,
+    result           TEXT
+);
+
